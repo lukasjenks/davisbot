8377dc8d1ca8d3db59d2410b7b498b9e0e8373cd
Lukas Jenks <lukasjenks@gmail.com>
2021-02-18 15:59:49 -0700

    Fix bugs in rewrite of pic add and pic like

--- picture.js
+++ picture.js
@@ -12,17 +12,14 @@ module.exports = {
                 }
                 break;
             case "like":
-                if (messageArr.length === 3) {
-                    this.fetchPicBySubString(msg, messageArr);
+                if (messageArr.length >= 3 && messageArr[2].charAt(0) === '"') {
+                    this.fetchPicBySubString(msg, messageArr[2]);
                 } else {
-                    msg.channel.send("Improper usage. Usage: !piclike [picture name]");
+                    msg.channel.send("Improper usage. Usage: !quote like \"quote substring here\"");
                 }
                 break;
             case "list":
                 this.listPics(msg);
-                break;
-            case "random":
-                this.fetchRandomPic(msg, messageArr);
                 break;
             case "show":
                 if (messageArr.length === 3) {
@@ -47,45 +44,47 @@ module.exports = {
                 } else {
                     msg.channel.send("Successfully added picture/gif to DB.");
                 }
-            })
-            .catch((err) => {
-                msg.channel.send(`Promise error inserting into DB: Error: ${err}`);
             });
+            /*
+            .catch((err) => {
+                msg.channel.send(`Error inserting picture into DB: Error: ${err}`);
+            })
+            */
         }
     },
     fetchPicByName: function(msg, messageArr) {
-        var command = messageArr[1];
-        if (messageArr[1] === 'random') {
-            db.get('select * from picture', (err, row) => {
+        var command = messageArr[2];
+        if (command === 'random') {
+            db.get('select url from picture order by random() limit 1', (err, picRecord) => {
                 if (err) {
                     msg.channel.send("An error occured. Usage: !pic show [picture name]. Error: " + err.Error);
-                } else if (row !== undefined){
-                    msg.channel.send(row.url);
+                } else if (picRecord !== undefined) {
+                    msg.channel.send(picRecord.url);
                 } else {
                     msg.channel.send("No images found in the DB.");
                 }
             });
         } else {
-            db.get('select * from picture where command = ?', [command], (err, row) => {
+            db.get('select url from picture where command = ?', [command], (err, picRecord) => {
                 if (err) {
                     msg.channel.send("An error occured. Usage: !pic show [picture name]. Error: " + err.Error);
-                } else if (row !== undefined) {
-                    msg.channel.send(row.url);
+                } else if (picRecord !== undefined) {
+                    msg.channel.send(picRecord.url);
                 } else {
                     msg.channel.send("Picture/gif named not found in the DB.");
                 }
             });
         }
     },
-    fetchPicBySubString: function(msg, messageArr) {
-        var command = messageArr[1];
-        db.get ('select * from picture where command like ?', '%' + command + '%', (err, row) => {
+    fetchPicBySubString: function(msg) {
+        var subStr = msg.content.split(/"/)[1];
+        db.get('select url from picture where command like ? order by random() limit 1', '%' + subStr + '%', (err, picRecord) => {
             if (err) {
-                msg.channel.send("An error occured. Usage: !piclike [picture name substring]. Error: " + err.Error);
-            } else if (row !== undefined) {
-                msg.channel.send(row.url);
+                msg.channel.send("An error occured. Usage: !quotelike \"quote substring here\". Error: " + err.Error);
+            } else if (picRecord !== undefined) {
+                msg.channel.send(picRecord.url);
             } else {
-                msg.channel.send("No picture with a name similar to what was given was found in the DB.");
+                msg.channel.send("No quote which contains the substring specified was found in the DB.");
             }
         });
     },
