48a4b1dbdac810a0398c03c7dcd7abdd998ddc03
Zachary Shaw <zachthm@gmail.com>
2021-06-26 13:55:17 -0600

    Add Emoji Multiplier * Add feature that allows users to multiply emojis * Example usage: ':emojiname * 10' causes the bot to message the same emoji that many times
    * Note: This only works for built in emojis and server specific ones

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const auth = require('./auth.json');
 const token = auth.token;
+
 const MIN_INTERVAL = 2 * 60 * 1000;
 
 // Import helper function modules
@@ -9,27 +10,27 @@ const author = require('./src/author');
 const picture = require('./src/picture');
 const utils = require('./src/utils');
 const update = require('./src/update');
+const emoji = require('./src/emoji');
 
 const client = new Discord.Client();
 
 client.login(token)
-.catch((err) => {
-    console.log("An error occured: " + err);
-});
+    .catch((err) => {
+        console.log("An error occured: " + err);
+    });
 
 // Upon bot connection
 client.on('ready', () => {
-	console.log("Connected to Discord.");
+    console.log("Connected to Discord.");
 });
 
 // Handle comands
 client.on('message', (msg) => {
     // Command Mode
-	if (msg.content.charAt(0) === '!') {
-
+    if (msg.content.charAt(0) === '!') {
         // Split on whitespace for parsing of command
-        var messageArr = msg.content.split(/(\s+)/).filter( function(e) { return e.trim().length > 0; } );
-        
+        var messageArr = msg.content.split(/(\s+)/).filter(function (e) { return e.trim().length > 0; });
+
         switch (messageArr[0]) {
             case "!author":
                 author.authorCommand(msg, messageArr);
@@ -50,12 +51,17 @@ client.on('message', (msg) => {
                 break;
         }
     } else {
-        var messageArr = msg.content.split(/(\s+)/).filter( function(e) { return e.trim().length > 0; } );
-		if (!msg.author.bot) {
-            msg.content = msg.content.toLowerCase();
+        var messageArr = msg.content.split(/(\s+)/).filter(function (e) { return e.trim().length > 0; });
+        if (!msg.author.bot) {
+
+            emoji.multiply(msg, client);
+
+            // msg.content = msg.content.toLowerCase();
+
+
             // Here, you can check to see if msg.content.indexOf("custom string") returns > -1 to check for something
-			// that was said by a user, or msg.author.id to determine the author of the message
-			// and then take the desired action based on this. Lots of potential for fun stuff here.
-		}
+            // that was said by a user, or msg.author.id to determine the author of the message
+            // and then take the desired action based on this. Lots of potential for fun stuff here.
+        }
     }
 });
