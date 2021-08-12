d4945f2c051be02e318b37bcefafd2ae232a4dbe
Lukas Jenks <lukasjenks@gmail.com>
2021-08-12 12:05:31 -0600

    Implement \!temp now command

---
+++ src/cmd/temp.js
@@ -0,0 +1,108 @@
+const Discord = require('discord.js');
+const utils = require('../lib/utils');
+const axios = require("axios").default;
+const authInfo = require("../../auth.json");
+
+const outputTemp = (date, channel) => {
+	
+    let options = {
+        method: 'GET',
+		params: {lat: "53.5461",
+				 lon: "-113.4938",
+				 exclude: "minutely,hourly,alerts",
+				 units: "metric",
+				 appid: authInfo.openWeatherMapToken}
+    };
+
+	// e.g. https://api.openweathermap.org/data/2.5/onecall?lat=53.5461&lon=-113.4938&exclude=minutely,hourly,alerts&units=metric&appid=[apiKey]
+    axios.get("https://api.openweathermap.org/data/2.5/onecall", options).then(response => {
+		// response.data.current, response.data.daily.dt = unix epoch time
+		//86400 epoch unix time seconds in a day
+		let now = new Date().valueOf();
+		// Current data is structured differently in response JSON so must be parsed differently
+		if (response.data.current.sunrise < now < response.data.current.sunset) {
+			let entry = response.data.current;
+			const embed = new Discord.RichEmbed()
+				.setTitle("Forecast")
+				.setAuthor("OpenWeatherMap", "https://upload.wikimedia.org/wikipedia/commons/f/f6/OpenWeather-Logo.jpg")
+				.setColor('#00c0f5')
+				.addField("Temperature", entry.temp)
+				.addField("Feels Like", entry.feels_like)
+				.addField("Humidity", entry.humidity)
+				.addField("Wind Speed", entry.wind_speed)
+				.addField("Description", entry.weather.main + "(" + entry.weather.description + ")")
+			channel.send(embed);
+		} else {
+			// Handle tomorrow and all other days here
+		}
+	}).catch(error => {
+		console.log(error);
+		channel.send("An error occured. Error: " + error);
+	});
+}
+			/*
+			    "timezone": "America/Edmonton",
+  "timezone_offset": -21600,
+  "current": {
+    "dt": 1628785747,
+    "sunrise": 1628770134,
+    "sunset": 1628824150,
+    "temp": 18.51,
+    "feels_like": 17.8,
+    "pressure": 1021,
+    "humidity": 53,
+    "dew_point": 8.76,
+    "uvi": 2.74,
+    "clouds": 20,
+    "visibility": 10000,
+    "wind_speed": 2.06,
+    "wind_deg": 290,
+    "weather": [
+      {
+        "id": 801,
+        "main": "Clouds",
+        "description": "few clouds",
+        "icon": "02d"
+      }
+    ]
+  },
+  */
+
+// Returns current time in epoch seconds
+const getDateFromCmdFields = (fields) => {
+	if (fields[1] === undefined || fields[1] === "now") {
+		return new Date().valueOf();
+	} else if (fields[1] === "tomorrow") {
+		const today = new Date();
+		const tomorrow = new Date(today);
+		tomorrow.setDate(tomorrow.getDate() + 1);
+		return tomorrow.valueOf();
+	} else if (fields[1] === "next") {
+		const days = {"monday": 1, "tuesday": 2, "wednesday": 3, "thursday": 4, "friday": 5, "saturday": 6, "sunday": 7};
+		let d = new Date();
+		const day = d.getDay();
+		const targetDay = days[fields[2]];
+		let dayOffset = targetDay - day;
+		dayOffset < 0 ? dayOffset += 7 : null;
+		d = new Date(d.getTime() + (dayOffset * 24 * 3600 * 1000));
+		return d.valueOf();
+	}
+}
+
+// Wrap cmdHandler function in this way such that it can be called by building a dynamically generated
+// string of the function name in bot.js
+const cmdHandler = (msgInfo) => {
+    // Extract with regex
+    // !define [term]
+    const fields = msgInfo.content.match(msgInfo.regex.tempCmd);
+    
+    if (fields === null) {
+        utils.invalidUsage("!temp", msgInfo.channel);
+        return;
+    }
+
+	let date = getDateFromCmdFields(fields);
+	outputTemp(date, msgInfo.client);
+}
+
+module.exports = { cmdHandler };
--- src/lib/regex.js
+++ src/lib/regex.js
@@ -1,10 +1,27 @@
 const largeRegex = require('../cfg/large-regex.json');
 
+// Alphabetized by command
 module.exports = 
 {
+	ascii: {
+        validFiglet: new RegExp(/^[0-9a-zA-Z=!\?\s]+$/)
+    },
+	
     author: {
         authorAddCmd: new RegExp(/^\s*!author\s+(add)\s+([^\s]+)\s+([^\s]+)\s+([^\s]{1}.+[^\s]{1})$/),
         authorListCmd: new RegExp(/^\s*!author\s+(list)\s*$/)
+    },
+	
+    emoji: {
+        emojiCmd: new RegExp(/(?:^\s*[^\s]+\s*\*\s*[0-9]+\s*$)|(?:^\s*[0-9]+\s*\*\s*[^\s]+\s*$)/),
+        leftEmojiCmd: new RegExp(/^\s*([^\s]+)\s*\*\s*([0-9]+)\s*$/),
+        rightEmojiCmd: new RegExp(/^\s*([0-9]+)\s*\*\s*([^\s]+)\s*$/),
+        genericEmoji: new RegExp(Buffer.from(largeRegex.genericEmojiBase64, "base64").toString()),
+        discordEmoji: new RegExp(/^<:.+:([0-9]+)>$/)
+    },
+
+    define: {
+        defineCmd: new RegExp(/^\s*!define\s+(?:(?:([0-9]*)\s+)|.{0})([^\s]{1}.*[^\s]{1})$/)
     },
 
     pic: {
@@ -21,26 +38,15 @@ module.exports =
         quoteLikeCmd: new RegExp(/^\s*!quote\s+(like)\s+([^\s]{1}.+[^\s]{1})$/)
     },
 
+	temp: {
+		// !temp ([none]|now|tomorrow|next) ([none]|[day of week])
+		tempCmd: new RegExp(/^\s*!temp(?:\s+|.{0})((?:now)|(?:tomorrow)|(?:next).{0})((?:monday)|(?:tuesday)|(?:wednesday)|(?:thursday)|(?:friday)|(?:saturday)|(?:sunday)|.{0})\s*$/)
+	},
+
     update: {
         updateCmd: new RegExp(/^\s*!update\s+(author|pic)\s+(name|pic|url)\s+([^\s]+)\s+([^\s]{1}.+[^\s]{1})$/),
         updatePicCmd: new RegExp(/^\s*!update\s+(pic)\s+(name|url)\s+([^\s]+)\s+([^\s]+)\s*$/),
         updateAuthorNameCmd: new RegExp(/^\s*!update\s+(author)\s+(name)\s+([^\s]+)\s+([^\s]{1}.+[^\s]{1})$/),
         updateAuthorPicCmd: new RegExp(/^\s*!update\s+(author)\s+(pic)\s+([^\s]+)\s+([^\s]+)\s*$/)
-    },
-
-    emoji: {
-        emojiCmd: new RegExp(/(?:^\s*[^\s]+\s*\*\s*[0-9]+\s*$)|(?:^\s*[0-9]+\s*\*\s*[^\s]+\s*$)/),
-        leftEmojiCmd: new RegExp(/^\s*([^\s]+)\s*\*\s*([0-9]+)\s*$/),
-        rightEmojiCmd: new RegExp(/^\s*([0-9]+)\s*\*\s*([^\s]+)\s*$/),
-        genericEmoji: new RegExp(Buffer.from(largeRegex.genericEmojiBase64, "base64").toString()),
-        discordEmoji: new RegExp(/^<:.+:([0-9]+)>$/)
-    },
-
-    define: {
-        defineCmd: new RegExp(/^\!define\s+(?:(?:([0-9]*)\s+)|.{0})([^\s]{1}.*[^\s]{1})$/)
-    },
-
-    ascii: {
-        validFiglet: new RegExp(/^[0-9a-zA-Z=!\?\s]+$/)
     }
 }
